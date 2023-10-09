c18b7e1e38feb45a88e20bd7a254b09ed0d5a240
SirishaGurram5300 <125555949+SirishaGurram5300@users.noreply.github.com>
2023-10-09 19:29:16 +0530

    Add files via upload

---
+++ print self data.py
@@ -0,0 +1,8 @@
+print('my name is Sirisha')
+print('my fname is Jogarao')
+print('my mname is Jyothi')
+print('I am studying II datascience')
+print('my habbits are listening music and singing songs')
+print('I like to stay in church in my free time')
+print('my favourite color is blue')
+      
